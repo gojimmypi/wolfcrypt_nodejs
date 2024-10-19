b3912ace9ef39d4585a85c741635c908aee06c3e
gojimmypi <jim@wolfssl.com>
2024-10-18 21:05:47 -0700

    add console app (not working)

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ProcessStartInfo startInfo = new ProcessStartInfo("node", "server.js");
+        startInfo.UseShellExecute = false;
+        startInfo.RedirectStandardOutput = true;
+
+        using (Process process = Process.Start(startInfo))
+        {
+            using (System.IO.StreamReader reader = process.StandardOutput)
+            {
+                string result = reader.ReadToEnd();
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
