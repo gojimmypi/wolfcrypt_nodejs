161b23a442736061d56c85860b98430333410772
gojimmypi <jim@wolfssl.com>
2024-10-24 10:14:34 -0700

    initial setup script

---
+++ setup_env.ps1
@@ -0,0 +1,55 @@
+$ErrorActionPreference = "Stop"
+
+# Predefined Community, Professional, Enterprise versions of Visual Studio 2022
+$vsPathCommunity = "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
+$vsPathProfessional = "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\devenv.exe"
+$vsPathEnterprise = "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\devenv.exe"
+
+if (Test-Path "C:/workspace/wolfssl-$env:USERNAME") {
+    Write-Output "Found wolfSSL at C:/workspace/wolfssl-$env:USERNAME"
+    $env:WOLFSSL_LIB_PATH = "C:/workspace/wolfssl-$env:USERNAME/DLL Release/x64"
+    $env:WOLFSSL_INCLUDE_PATH = "C:/workspace/wolfssl-$env:USERNAME"
+    $env:WOLFSSL_USER_SETTINGS_PATH = "C:/workspace/wolfssl-$env:USERNAME/IDE/WIN"
+
+} elseif (Test-Path "C:/workspace/wolfssl") {
+    Write-Output "Found wolfSSL at C:/workspace/wolfssl"
+    $env:WOLFSSL_LIB_PATH = "C:/workspace/wolfssl/DLL Release/x64"
+    $env:WOLFSSL_INCLUDE_PATH = "C:/workspace/wolfssl"
+    $env:WOLFSSL_USER_SETTINGS_PATH = "C:/workspace/wolfssl/IDE/WIN"
+
+} else {
+    Write-Output "Could not find wolfSSL source directory."
+}
+
+
+Write-Output "WOLFSSL_LIB_PATH:           $($env:WOLFSSL_LIB_PATH)"
+Write-Output "WOLFSSL_INCLUDE_PATH:       $($env:WOLFSSL_INCLUDE_PATH)"
+Write-Output "WOLFSSL_USER_SETTINGS_PATH: $($env:WOLFSSL_USER_SETTINGS_PATH)"
+
+fnm env --use-on-cd | Out-String | Invoke-Expression
+fnm use --install-if-missing 20
+node -v # should print `v20.18.0`
+npx -v # should print `10.8.2`
+
+# Launch VS2022 from the same shell:
+# Check if the file exists
+if (Test-Path $vsPathEnterprise) {
+    # Run the file
+    Write-Output "Launching Visual Studio 2022 Enterprise at: $vsPathEnterprise"
+    Start-Process $vsPathEnterprise
+
+} elseif (Test-Path $vsPathProfessional) {
+    # Run the file
+    Write-Output "Launching Visual Studio 2022 Professional at: $vsPathProfessional"
+    Start-Process $vsPathProfessional
+
+} elseif (Test-Path $vsPathCommunity) {
+    # Run the file
+    Write-Output "Launching Visual Studio 2022 Community at: $vsPathCommunity"
+    Start-Process $vsPathCommunity
+
+} else {
+    Write-Output "Visual Studio 2022 executable not found."
+}
+
+cd C:\workspace\wolfcrypt_nodejs-gojimmypi
